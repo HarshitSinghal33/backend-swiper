b8c77cc370e433e62267e482663271e383df06f1
Harshit Singhal <harshit.singhal@coderower.com>
2025-04-28 07:20:24 +0530

    reafctor: increase rate limit

--- src/middleware/rateLimiter.js
+++ src/middleware/rateLimiter.js
@@ -2,7 +2,7 @@ import rateLimit from 'express-rate-limit';
 
 export const rateLimiter = rateLimit({
     windowMs: 60 * 1000 ,
-    max: 50,
+    max: 100,
     message: "Too many requests, please try again later.",
     skip: (req) => req.method === "OPTIONS"
 })
